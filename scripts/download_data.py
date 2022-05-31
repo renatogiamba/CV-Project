814ec59635d68c7e3f9c83ce122655b593ed67ae
lorenzo-di-luccio <diluccio.1797569@studenti.uniroma1.it>
2022-05-31 14:43:50 +0200

    Completed DIV2K dataset download script

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -5,7 +5,33 @@ import torchvision
 import torchvision.datasets
 import torchvision.datasets.utils
 
+
 DIV2K_DATASET_HOME_URL = "https://data.vision.ee.ethz.ch/cvl/DIV2K/"
+DIV2K_DATASET_BRANCHES_ARCHIVES = {
+    "train_LR": "DIV2K_train_LR_unknown_X4.zip",
+    "train_HR": "DIV2K_train_HR.zip",
+    "valid_LR": "DIV2K_valid_LR_unknown_X4.zip",
+    "valid_HR": "DIV2K_valid_HR.zip"
+}
+DIV2K_DATASET_BRANCHES_FOLDERS = {
+    "train_LR": "DIV2K_train_LR",
+    "train_HR": "DIV2K_train_HR",
+    "valid_LR": "DIV2K_valid_LR",
+    "valid_HR": "DIV2K_valid_HR"
+}
+DIV2K_DATASET_BRANCHES_URLS = {
+    "train_LR": DIV2K_DATASET_HOME_URL +
+    DIV2K_DATASET_BRANCHES_ARCHIVES["train_LR"],
+
+    "train_HR": DIV2K_DATASET_HOME_URL +
+    DIV2K_DATASET_BRANCHES_ARCHIVES["train_HR"],
+
+    "valid_LR": DIV2K_DATASET_HOME_URL +
+    DIV2K_DATASET_BRANCHES_ARCHIVES["valid_LR"],
+
+    "valid_HR": DIV2K_DATASET_HOME_URL +
+    DIV2K_DATASET_BRANCHES_ARCHIVES["valid_HR"]
+}
 
 
 def download_and_decompress(
@@ -62,34 +88,69 @@ def download_and_decompress(
 
 
 if __name__ == "__main__":
-    # set the CLI commands and options
-    div2k_cli = argparse.ArgumentParser()
+    # set the general CLI commands and options
+    cli = argparse.ArgumentParser()
+    sub_clis = cli.add_subparsers()
+
+    # set the CLI commands and options for the DIV2K dataset
+    div2k_cli = sub_clis.add_parser(
+        "div2k", help="wheter to download the DIV2K dataset or not")
+    div2k_cli.set_defaults(dataset="div2k")
     div2k_cli.add_argument(
         "--download_root", action="store", default="../data/",
         help="the folder where the data are downloaded in")
     div2k_cli.add_argument(
+        "--train_LR", action="store_true", default=False,
+        help="wheter to download the training set of LR images or not")
+    div2k_cli.add_argument(
         "--train_HR", action="store_true", default=False,
         help="wheter to download the training set of HR images or not")
+    div2k_cli.add_argument(
+        "--valid_LR", action="store_true", default=False,
+        help="wheter to download the validation set of LR images or not")
     div2k_cli.add_argument(
         "--valid_HR", action="store_true", default=False,
         help="wheter to download the validation set of HR images or not")
 
     # MORE TO COME HERE! NOW ONLY DIV2K
 
+    # set the CLI commands and options for the Set14 dataset
+    set14_cli = sub_clis.add_parser(
+        "set14", help="wheter to download the Set14 dataset or not")
+    set14_cli.set_defaults(dataset="set14")
+
     # parse the CLI and store the commands/options in a sort of dictionary
-    args = div2k_cli.parse_args()
+    args = cli.parse_args()
 
     # download options
-    if args.train_HR:
-        download_and_decompress(
-            "DIV2K", "train_HR",
-            DIV2K_DATASET_HOME_URL + "DIV2K_train_HR.zip",
-            "../data/DIV2K_train_HR.zip", "../data/DIV2K_train_HR",
-            args.download_root)
-    if args.valid_HR:
-        download_and_decompress(
-            "DIV2K", "valid_HR",
-            DIV2K_DATASET_HOME_URL + "DIV2K_valid_HR.zip",
-            "../data/DIV2K_valid_HR.zip", "../data/DIV2K_valid_HR",
-            args.download_root
-        )
+    if args.dataset == "div2k":
+        if args.train_LR:
+            download_and_decompress(
+                "DIV2K", "train_LR",
+                DIV2K_DATASET_BRANCHES_URLS["train_LR"],
+                f"../data/{DIV2K_DATASET_BRANCHES_ARCHIVES['train_LR']}",
+                f"../data/{DIV2K_DATASET_BRANCHES_FOLDERS['train_LR']}",
+                args.download_root)
+        if args.train_HR:
+            download_and_decompress(
+                "DIV2K", "train_HR",
+                DIV2K_DATASET_BRANCHES_URLS["train_HR"],
+                f"../data/{DIV2K_DATASET_BRANCHES_ARCHIVES['train_HR']}",
+                f"../data/{DIV2K_DATASET_BRANCHES_FOLDERS['train_HR']}",
+                args.download_root)
+        if args.valid_LR:
+            download_and_decompress(
+                "DIV2K", "valid_LR",
+                DIV2K_DATASET_BRANCHES_URLS["valid_LR"],
+                f"../data/{DIV2K_DATASET_BRANCHES_ARCHIVES['valid_LR']}",
+                f"../data/{DIV2K_DATASET_BRANCHES_FOLDERS['valid_LR']}",
+                args.download_root)
+        if args.valid_HR:
+            download_and_decompress(
+                "DIV2K", "valid_HR",
+                DIV2K_DATASET_BRANCHES_URLS["valid_HR"],
+                f"../data/{DIV2K_DATASET_BRANCHES_ARCHIVES['valid_HR']}",
+                f"../data/{DIV2K_DATASET_BRANCHES_FOLDERS['valid_HR']}",
+                args.download_root)
+    elif args.dataset == "set14":
+        pass
